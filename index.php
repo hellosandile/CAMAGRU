9bccb09f81ec6af9e628f7f0f0cb90f12df42a7b
Sandile MKHIZE <samkhize@c5r1s1.wethinkcode.co.za>
2019-10-23 13:58:40 +0200

    rename

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 <h2>User Authentication System </h2><hr>
 </html>
 
-<p>You are currently not signed in <a href="/login.php">Login</a> Not yet a member? <a href="signup.php">Sign up</a></p>
+<p>You are currently not signed in <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Sign up</a></p>
 
-<p>You are loggin in as {username} <a href="logout.php">Logout</a></p>
+<p>You are loggin in as {username} <a href="login.php">Logout</a></p>
 </body>
 </html>
