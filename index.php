b3934138e675d576183144f938c5cc9925a3d9a7
hellosandile <mkhizenatha@gmail.com>
2019-10-28 19:48:52 +0200

    Monday

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>Homepage</title>
-</head>
-<body>
-<h2>User Authentication System </h2><hr>
-</html>
+<?php
+require_once 'core/init.php';
 
-<p>You are currently not signed in <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Sign up</a></p>
-
-<p>You are loggin in as {username} <a href="signup.php">Logout</a></p>
-</body>
-</html>
+DB::getInstance();
--- resource/db.php
+++ resource/db.php
@@ -1,19 +1,30 @@
 <?PHP
 
-$username = 'root';
-$dsn = 'mysql:host=localhost; dbname=register';
-$password = '';
+class DB {
+    //Will store the instance of the DB if it is available
+    private static $_instance = NULL;
+    private $_pdo,
+            $_query,
+            $_error = false,
+            $results,
+            $_count = 0;
 
+    private function _construct() {
+        try {
+            $this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+            echo 'Connected';
+        }catch(PDOException $e) {
+            die($e->getMessage());
+        }
+        
+    }
 
-try{
-    $db = new PDO($dsn, $username, $password);
-    
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    echo "Connected to the register database";
+    public static function getInstance() {
+        if(!isset(self::$_instance)) {
+            self::$_instance = new DB();
+        }
+        return self::$_instance;
+    }
 
-}catch (PDOException $ex){
-    
-    echo "Connection failed" . $ex->getMessage();
 
 }
