b000696bb4cb8a5657e372a4b09bde69a83ece10
Sandile MKHIZE <samkhize@c5r1s1.wethinkcode.co.za>
2019-11-25 12:25:07 +0200

    Morning work

--- gallery.php
+++ gallery.php
@@ -1,112 +1,61 @@
 <?php
-  include 'functions/session.php';
-  include_once 'config/database.php';
+include 'functions/session.php';
+include_once 'config/database.php';
+ 
+
+//--- SET THE VARIABLEA 
+// try {
+//     $con = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+//         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+//         $sql = "USE ".$DB_NAME;
+//                 $sql = "SELECT * FROM pictures_table WHERE user_id = 8";
+//                 $stmt = $con->prepare($sql);
+//                 $stmt->bindParam(1, $_GET['user_id']);
+//                 $stmt->execute();
+ 
+
+ 
+// $num = $stmt->rowCount();
+ 
+// if( $num ){
+//     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+//     header("Content-type: image/png");
+//     print $row['data'];
+//     exit;
+// }else{
+//     echo "nothing";
+// }}catch(PDOException $e)
+// {
+//     echo $stmt . "<br>" . $e->getMessage();
+// }
+// $con = null;
 ?>
 
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Image Upload</title>
-    <link rel="stylesheet" href="styling.css"/>
+
 </head>
 <body>
-
-<div>
-<?php include 'includes/header.php' ?>
-</div>
-<br/>
 <div id="content">
-  <?php
+<?php
     try {
-          $con = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
-              $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-              $sql = "USE ".$DB_NAME;
-                      $sql = "SELECT * FROM pictures_table";
-                      $stmt = $con->prepare($sql);
-                      $stmt->execute();
-                      //$res = $stmt->fetch();
-        $html = "<ul id = 'taken-pics'>";
-        $img_dir = "img_gallery/";
-        //echo $res['image'];
-        while( $res = $stmt->fetch()) 	
-        { 
-          $sql = "USE ".$DB_NAME;
-                      $sql = "SELECT * FROM `comments` WHERE `user_id`= 8";
-                      $st= $con->prepare($sql);
-                      $st->execute();
-
-          $img  = $res['image'];
-          if($img=== '.' || $img === '..') 
-          {
-            continue;
-          } 		   
-          if (preg_match('/.png/',$img) )
-          {				
-            $html .="<li>
-            <div style = '  width : 100%;
-            height : 120;  display : inline-block;
-            z-index : 10;
-            padding-right : 2%;'>
-            <img style='  width : 200;
-            height : 120;
-            z-index : 10;
-            display : inline-block;
-            float : left;
-            border-right : 1px solid #f5f7f6;' src='".$img_dir.$img."'/>
-            
-            <form action = 'comment.php' method = 'post'>
-            <textarea 
-      	id='text' 
-      	cols='40' 
-      	rows='4'
-      	name='image_text' 
-        placeholder='Add a comment...'></textarea>
-        <div style='  width : 200;
-        height : 120;
-        z-index : 10;
-        display : inline-block;
-        border-right : 1px solid darkgrey;'></div>
-        <br/>
-        
-            <input type = 'hidden' name = 'img_id' value = '".$res['photo_id']."'/>
-            <input type = 'submit' name = 'comment' value = 'comment'/>
-        </form>
-        <form action = 'like.php' method = 'post'>
-            <input type = 'hidden' name = 'img_id' value = '".$res['photo_id']."'/>
-            <input type = 'submit' name = 'like' value = 'like'/>
-            </form>
-
-
-         "
-            while($out = $st->fetchAll())
-            {
-              var_dump($out);
-              echo $out['comment'];
-            }
-          "  
-          </div>
-            </div>
-            <div>
-            </li>";
-          
-          } 
-          else 
-          { 
-            continue;
-          }	
-        } 
-        $html .="</ul>" ; 
-        echo $html;
-      }catch(PDOException $e)
-      {
-          echo $stmt . "<br>" . $e->getMessage();
+        $con = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            $sql = "USE ".$DB_NAME;
+                    $sql = "SELECT * FROM pictures_table ORDER BY photo_id DESC";
+                    $stmt = $con->prepare($sql);
+                    $stmt->execute();
+      while( $res = $stmt->fetch()){
+          echo "<div id='img_div'>";
+          echo "<img src='img_gallery/".$res['image']."'>";
+          echo "</div>";
       }
-  $con = null;
-  ?>
-  
+    }catch(PDOException $e)
+    {
+        echo $stmt . "<br>" . $e->getMessage();
+    }
+?>
 </div>
-<div class="footer">
-        <?php include 'includes/footer.php' ?>
-        </div>
 </body>
 </html>
